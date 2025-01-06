d904ff5984123fb0e73302dc1507fd1746e3efb9
jobinjose <jose.jobiin@gmail.com>
2025-01-06 14:47:05 +0530

    feat: layout completed

--- hosman-frontend/src/routes/sections/auth.tsx
+++ hosman-frontend/src/routes/sections/auth.tsx
@@ -1,30 +1,71 @@
-// import Login from "./pages/login";
+import { lazy, Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { Outlet } from "react-router-dom";
-import { CenteredSignInView } from "src/sections/auth/view/sign-in-view";
-import { SignUpView } from "src/sections/auth/view/sign-up-view";
+import { GuestGuard } from 'src/guard';
+import { AuthCenteredLayout } from 'src/layouts/auth-centered';
+import { AuthSplitLayout } from 'src/layouts/auth-split';
+
+import { SplashScreen } from 'src/components/loading-screen';
+
+const SignInPage = lazy(() => import('src/pages/auth/sign-in'));
+const SignUpPage = lazy(() => import('src/pages/auth/sign-up'));
+// const ForgotPassword = lazy(() => import('src/pages/auth/forgot-password'));
+// const ResetPassword = lazy(() => import('src/pages/auth/reset-password'));
 
 export const authRoutes = [
   {
-    path: "/auth",
-    element:(
-      <Outlet/>
+    path: 'auth',
+    element: (
+      <Suspense fallback={<SplashScreen />}>
+        <Outlet />
+      </Suspense>
     ),
-    children:[
+    children: [
       {
-        path:"sign-in",
-        element:(
-          <CenteredSignInView/>
-        )
+        path: 'sign-in',
+        element: (
+          <GuestGuard>
+            <AuthCenteredLayout>
+              <SignInPage />
+            </AuthCenteredLayout>
+          </GuestGuard>
+        ),
       },
       {
-        path:"sign-up",
-        element:(
-          <SignUpView/>
-        )
+        path: 'sign-up',
+        element: (
+          <GuestGuard>
+            <AuthSplitLayout
+              section={{
+                title: 'Welcome to HosMan... For  yours!',
+                subtitle: 'Your gateway to a seamless and successful health consideration journey',
+              }}
+            >
+              <SignUpPage />
+            </AuthSplitLayout>
+          </GuestGuard>
+        ),
       },
-      
-    ]
+      // {
+      //   path: 'forgot-password',
+      //   element: (
+      //     <GuestGuard>
+      //       <AuthSplitLayout>
+      //         <ForgotPassword />
+      //       </AuthSplitLayout>
+      //     </GuestGuard>
+      //   ),
+      // },
+      // {
+      //   path: 'reset-password',
+      //   element: (
+      //     <GuestGuard>
+      //       <AuthSplitLayout>
+      //         <ResetPassword />
+      //       </AuthSplitLayout>
+      //     </GuestGuard>
+      //   ),
+      // },
+    ],
   },
-  
 ];
